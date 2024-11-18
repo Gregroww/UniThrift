50dffea4aac8c154408a5fe29d544512b4958e99
Cellia <celliaauzianugraha@gmail.com>
2024-11-19 01:28:52 +0800

    menambahkan file waiting list, style, dan img

---
+++ waiting_list1.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Waiting List (Waiting)</title>
+    <link rel="stylesheet" href="style/waitinglist.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/remixicon/2.5.0/remixicon.css">
+</head>
+
+<body>
+    <header>
+        <?php require "navbar.php"; ?>
+    </header>
+
+    <h1>STATUS PENGGUNA</h1>
+
+<div class="modal-container">
+    <div class="modal-box">
+        <img src="images/time-left 1.png" alt="">
+        <h3>PENGAJUAN PEMBUATAN<br> AKUN ANDA SEDANG<br> DIPROSES</h3>
+        <button class="back-to-home">KEMBALI KE PROFIL</button>
+    </div>
+</div>
+
+    <?php require "footer.php"; ?>
+</body>
+</html>
