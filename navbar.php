37e7eadc314cf398272831e85f56d25a70b8f726
Celio Arga <celioarga05@gmail.com>
2024-11-18 21:32:02 +0800

    Update navbar + up logout.php

--- navbar.php
+++ navbar.php
@@ -1,5 +1,14 @@
 <?php
+session_start();
 require 'connect.php';
+
+if (isset($_SESSION['nama_pengguna'])) {
+    $nama_pengguna = $_SESSION['nama_pengguna'];
+    $query = "SELECT foto_ktm FROM users WHERE nama_pengguna = '$nama_pengguna'";
+    $result = mysqli_query($conn, $query);
+    $user = mysqli_fetch_assoc($result);
+    $foto_ktm = $user['foto_ktm'];
+}
 ?>
 
 <!DOCTYPE html>
@@ -8,6 +17,7 @@ require 'connect.php';
     <link rel="stylesheet" href="navbar.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
 </head>
 <body>
@@ -21,15 +31,22 @@ require 'connect.php';
                 <input type="text" placeholder="Cari">
                 <input type="text" placeholder="Kota">
                 <button class="search-button">
-                    🔍
-            </button>
+                <i class="fa-solid fa-magnifying-glass"></i>
+                </button>
             </div>
-        </div>
-        
+        </div> 
         <div class="nav-links">
             <a href="index.php">Beranda</a>
-            <a href="tentang-kami">Tentang Kami</a>
-            <a href="login.php">Masuk</a>
+            <a href="tentang-kami.php">Tentang Kami</a>
+            <?php if(isset($_SESSION['nama_pengguna'])): ?>
+                <a href="logout.php">Keluar</a>
+                <a href="sellpage.php" class="jual-btn">Jual</a>
+                <a href="pageprofile.php">
+                <img src="assets/<?php echo $foto_ktm; ?>" alt="Foto Profil" class="foto-profil">
+                </a>
+            <?php else: ?>
+                <a href="login.php">Masuk</a>
+            <?php endif; ?>
         </div>
     </nav>
 </body>
