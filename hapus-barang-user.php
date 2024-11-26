8d49519a14b2194145d6fee6b5b7bb23601eacb0
ItsukiKyun <aimanzuhair05@gmail.com>
2024-11-26 22:00:38 +0800

    .

--- hapus-barang-user.php
+++ hapus-barang-user.php
@@ -2,7 +2,6 @@
 session_start();
 require 'connect.php';
 
-// Cek apakah user sudah login
 if (!isset($_SESSION['nama_pengguna'])) {
     header("Location: login.php");
     exit();
@@ -11,8 +10,6 @@ if (!isset($_SESSION['nama_pengguna'])) {
 if (isset($_POST['id_barang'])) {
     $id_barang = mysqli_real_escape_string($conn, $_POST['id_barang']);
     $nama_pengguna = $_SESSION['nama_pengguna'];
-
-    // Cek apakah barang milik user yang sedang login
     $check_query = "SELECT * FROM barang WHERE id_barang = '$id_barang' AND nama_pengguna = '$nama_pengguna'";
     $result = mysqli_query($conn, $check_query);
 
